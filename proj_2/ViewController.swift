a3ec8f173a86f8b0707ab2a2c542c6bef9bb58e2
Yue <yue@Rohits-MacbookPro.local>
2018-10-19 14:05:19 -0600

    Initial Commit

---
+++ proj_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proj_2
+//
+//  Created by Yue on 10/19/18.
+//  Copyright © 2018 Yue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ proj_2Tests/proj_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  proj_2Tests.swift
+//  proj_2Tests
+//
+//  Created by Yue on 10/19/18.
+//  Copyright © 2018 Yue. All rights reserved.
+//
+
+import XCTest
+@testable import proj_2
+
+class proj_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
