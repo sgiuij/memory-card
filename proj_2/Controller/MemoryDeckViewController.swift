6c8695b4e51f870fe6d65ba7a75ca06ee18476b1
Yue <yue@iPad.app.byu.edu>
2018-10-22 11:41:36 -0600

    first commit

---
+++ proj_2/Controller/MemoryDeckViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MemoryDeckViewController.swift
+//  proj_2
+//
+//  Created by Yue on 10/22/18.
+//  Copyright © 2018 Yue. All rights reserved.
+//
+
+import Foundation
---
+++ proj_2/View/StartPageView.swift
@@ -0,0 +1,9 @@
+//
+//  StartPageView.swift
+//  proj_2
+//
+//  Created by Yue on 10/22/18.
+//  Copyright © 2018 Yue. All rights reserved.
+//
+
+import Foundation
